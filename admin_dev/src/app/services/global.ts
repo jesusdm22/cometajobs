7ea28d96f50ac970666796c44147094c8e47eda2
unknown <jesusdm22@hotmail.com>
2020-06-07 20:10:02 +0200

    Firmado digital de los documentos

--- admin_dev/src/app/services/global.ts
+++ admin_dev/src/app/services/global.ts
@@ -1,4 +1,4 @@
 export var GLOBAL = {
-    //url: 'https://api.cometajobs.com/api/' //URL de la API (debe de estar corriendo);
-    url: 'http://localhost:3800/api/'
+    url: 'https://api.cometajobs.com/api/' //URL de la API (debe de estar corriendo);
+    //url: 'http://localhost:3800/api/'
 }
