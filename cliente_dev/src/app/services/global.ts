e92a34ce3eaf7f43d2330730f44c957c92112610
unknown <jesusdm22@hotmail.com>
2020-06-07 21:41:06 +0200

    Actualizacion campo contraseña

--- cliente_dev/src/app/services/global.ts
+++ cliente_dev/src/app/services/global.ts
@@ -1,4 +1,4 @@
 export var GLOBAL = {
-    //url: 'https://api.cometajobs.com/api/' //URL de la API (debe de estar corriendo);
-    url: 'http://localhost:3800/api/' //URL de la API (debe de estar corriendo);
+    url: 'https://api.cometajobs.com/api/' //URL de la API (debe de estar corriendo);
+    //url: 'http://localhost:3800/api/' //URL de la API (debe de estar corriendo);
 }
