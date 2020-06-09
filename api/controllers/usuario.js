f87a29f72c05e1a3b8f8db3be8dbc7fa50f7d74c
unknown <jesusdm22@hotmail.com>
2020-06-09 16:39:51 +0200

    Admin solucionado

--- api/controllers/usuario.js
+++ api/controllers/usuario.js
@@ -194,14 +194,13 @@ function deleteUser(req, res) {
 
     User.find({'_id': userId }).exec((err, usuario) => {
         if(usuario.login == "admin"){
-            return res.status(500).send({ message: 'No puedes eliminar al administrador principal' });
+            console.log("El usuario es admin!");
+            return res.status(500).send({ message: 'No puedes eliminar al administrador principal' });     
         } else {
             User.find({'_id': userId }).deleteOne((err, usuarioEliminado) => {
                 if (err) return res.status(500).send({ message: 'Error al intentar eliminar el usuario' });
         
                 if (!usuarioEliminado) return res.status(404).send({ message: 'El usuario no existe o ya ha sido eliminado' });
-                
-                //if(usuarioEliminado.login == 'admin') res.status(500).send({ message: 'No puedes eliminar al administrador principal' });
         
                 return res.status(200).send({ message: 'El usuario ha sido eliminado' });
             });
